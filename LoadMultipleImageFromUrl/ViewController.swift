af78834850616e86ed26998076c79b10a596666c
Chander bhushan <cbhushan1995@gmail.com>
2019-04-01 20:48:19 +0530

    Initial Commit

---
+++ LoadMultipleImageFromUrl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoadMultipleImageFromUrl
+//
+//  Created by chander bhushan on 01/04/19.
+//  Copyright © 2019 Educational. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
